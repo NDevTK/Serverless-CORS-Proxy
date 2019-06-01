4b32bcb5b83af257c410130590c51d428eab3168
NDevTK <31563761+NDevTK@users.noreply.github.com>
2019-06-01 16:47:07 +0100

    Exposed header X-Final-URL

--- index.js
+++ index.js
@@ -1,33 +1,34 @@
 const express = require('express');  
 const request = require('request');
-const cors = require('cors')
 const app = express();  
 
-app.use(function(req, res, next) { // Cache
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Expose-Headers", "X-Final-URL");
     res.header('Cache-Control', 'public, smax-age=600, max-age=600');
     next();
 });
 
-function isURL(str) {
+function isURL(str) {// id
   var regex = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/;
   var pattern = new RegExp(regex); 
   return pattern.test(str);
 }
 
-app.get('/', cors(), (req, res, next) => {
+app.get('/', (req, res, next) => {
   url = req.query.url;
-  if(!url){ // If no URL specified
+  if(!url){
 	  res.send("Hello World");
 	  return
   }
-  if(!isURL(url)){ // If URL is not valid
+  if(!isURL(url)){
 	  res.send("ERROR: URL NOT VALID")
 	  return;
   }
   
-  data = req.pipe(request(url)) // Get data
+  data = req.pipe(request(url))
   res.header('X-Final-URL', data.uri.href);
-  data.pipe(res); // Reply
+  data.pipe(res);
 });
 
 module.exports = app;
