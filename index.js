5982c2812a3778bb33365a3a75f58a3f57d2d386
Official Noob <31563761+OfficialNoob@users.noreply.github.com>
2019-06-01 20:28:20 +0100

    Added 504

--- index.js
+++ index.js
@@ -1,34 +1,37 @@
-const express = require('express');
+const express = require('express');  
 const request = require('request');
-const app = express();
+const app = express();  
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Expose-Headers", "X-Final-URL");
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Expose-Headers", "X-Final-URL");
     res.header('Cache-Control', 'public, smax-age=600, max-age=600');
     next();
 });
 
-function isURL(str) { // id
-    var regex = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/;
-    var pattern = new RegExp(regex);
-    return pattern.test(str);
+function isURL(str) {
+  var regex = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/;
+  var pattern = new RegExp(regex); 
+  return pattern.test(str);
 }
 
 app.get('/', (req, res, next) => {
-    url = req.query.url;
-    if (!url) {
-        res.send("Hello World");
-        return
-    }
-    if (!isURL(url)) {
-        res.send("ERROR: URL NOT VALID")
-        return;
-    }
+  url = req.query.url;
+  if(!url){
+	  res.send("Hello World");
+	  return
+  }
+  if(!isURL(url)){
+	  res.send("ERROR: URL NOT VALID")
+	  return;
+  }
 
   req.pipe(request.get(url).on('response', function(response) {
 	  res.header('X-Final-URL', response.request.uri.href);
+  }).on('error', function(err) {
+	  res.status(504).send("REMOTE ERROR");
   })).pipe(res);
+  
 });
 
 module.exports = app;
